cbfb4a31f375fd1dc19b072d3e905ef96b1507f8
Sérgio Correia <secorre@microsoft.com>
2023-12-14 14:18:22 +0000

    Add settings

--- src/pages/feedback/Feedback.tsx
+++ src/pages/feedback/Feedback.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./feedback.scss";
 
 const Feedback = () => {
--- src/pages/feedbacks/Feedbacks.tsx
+++ src/pages/feedbacks/Feedbacks.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./feedbacks.scss";
 
 const Feedbacks = () => {
